0616ffeb1ee539273050aa3bcca5e8605052c86b
jzc777 <jzchen7@foxmail.com>
2023-09-27 20:50:09 +0800

    Add testSubtraction.java

---
+++ src/test/java/org/example/testSubtraction.java
@@ -0,0 +1,13 @@
+package org.example;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class testSubtraction {
+    Calc c = new Calc();
+
+    @Test
+    void testAddition() {
+        assertEquals(2, c.subtract(4,2));
+    }
+
+}
